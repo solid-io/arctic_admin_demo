251ede3c9a450acf5c1ea1fd999bc3ac28152be5
Jason Rossi <jason.rossi@epi.io>
2021-10-29 23:17:46 -0600

    add schedule active admin resource

---
+++ app/admin/schedules.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Schedule do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :scheduleable_id, :scheduleable_type, :name, :active, :capacity, :user_types, :exclude_lunch_time, :lunch_hour_start, :lunch_hour_end, :beginning_of_week, :time_zone
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:scheduleable_id, :scheduleable_type, :name, :active, :capacity, :user_types, :exclude_lunch_time, :lunch_hour_start, :lunch_hour_end, :beginning_of_week, :time_zone]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
