b4980411ca4579ad612e913fa89b2849eb1b35ab
Jason Rossi <jason.rossi@epi.io>
2021-10-30 01:13:37 -0600

    implement app wide timezone handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  around_action :set_time_zone, if: :current_admin_user
+
+  private
+
+  def set_time_zone(&block)
+    Time.use_zone(current_admin_user.time_zone, &block)
+  end
 end
