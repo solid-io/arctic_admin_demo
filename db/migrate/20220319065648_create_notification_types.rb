210b00ba9f2a5b138e1e19c96768f828f162d5e9
Jason Rossi <jason.rossi@epi.io>
2022-03-19 01:07:50 -0600

    added notification type

---
+++ db/migrate/20220319065648_create_notification_types.rb
@@ -0,0 +1,17 @@
+class CreateNotificationTypes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notification_types do |t|
+      t.string :name
+      t.boolean :email_enabled, default: true
+      t.string :mailer
+      t.string :email
+      t.boolean :push_enabled, default: true
+      t.string :push_summary
+      t.text :push_details
+      t.boolean :sms_enabled, default: false
+      t.text :sms_body
+
+      t.timestamps
+    end
+  end
+end
