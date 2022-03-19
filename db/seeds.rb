7b840e2e75c94d97a3e306390944b5c50722eb22
Jason Rossi <jason.rossi@epi.io>
2022-03-19 01:13:53 -0600

    add notification type seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,6 @@
   schedule = Schedule.create!(name: "Appointments", time_zone: "Mountain Time (US & Canada)",  beginning_of_week: "sunday", exclude_lunch_time: false, lunch_hour_start: "", lunch_hour_end: "")
   schedule.rules.create!(rule_type: "inclusion", name: "Open Time Rule MWF", frequency_units: "IceCube::MinutelyRule", frequency: 15, days_of_week: ["monday","wednesday","friday"], start_date: Date.today, end_date: Date.today + 90.days, rule_hour_start: "08:00", rule_hour_end: "20:00")
   schedule.rules.create!(rule_type: "inclusion", name: "Open Time Rule TTH", frequency_units: "IceCube::MinutelyRule", frequency: 15, days_of_week: ["tuesday","thursday"], start_date: Date.today + 1.week, end_date: Date.today + 90.days, rule_hour_start: "17:15", rule_hour_end: "23:45")
+
+  NotificationType.create!(name: "ScheduleMailer.update", email_enabled: true, mailer: "ScheduleMailer", email: "update", push_enabled: true, push_summary: "The schedule has been updated.", push_details: "Updated Schedule", sms_enabled: false, sms_body: "")
 end
