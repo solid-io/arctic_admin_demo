68109982ab653c4d765c60cbd2beecb0447a2a36
Jason Rossi <jason.rossi@epi.io>
2022-03-24 13:24:03 -0600

    add phoneable

---
+++ app/models/phone.rb
@@ -0,0 +1,10 @@
+class Phone < ApplicationRecord
+  belongs_to :phoneable, polymorphic: true
+
+  before_save :process_phone_number
+
+  def process_phone_number
+    self.is_valid = Utils::Phone.validate(phone_number)["valid"]
+    self.phone_number = Utils::Phone.normalize(phone_number)
+  end
+end
