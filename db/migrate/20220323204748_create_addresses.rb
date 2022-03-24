7089ff2fb53e07b50742c4fd4025b62146387424
Jason Rossi <jason.rossi@epi.io>
2022-03-24 13:23:14 -0600

    add addressable

---
+++ db/migrate/20220323204748_create_addresses.rb
@@ -0,0 +1,16 @@
+class CreateAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addresses do |t|
+      t.references :addressable, polymorphic: true, null: false
+      t.string :label
+      t.string :address_line_1
+      t.string :address_line_2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.boolean :default, default: true
+
+      t.timestamps
+    end
+  end
+end
