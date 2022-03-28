f62d1002ee5b259edd5f8fe7fde01453f2f4ccd8
Jason Rossi <jason.rossi@epi.io>
2022-03-27 21:07:53 -0600

    add should and simplecov gems enable used company_test

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
-
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = companies(:example_company)
+  end
+
+  context 'associations' do
+    should have_one_attached(:logo)
+    should have_many(:locations)
+    should have_many(:admin_user_companies)
+    should have_many(:admin_users).through(:admin_user_companies)
+    should accept_nested_attributes_for(:locations)
+  end
+
+  context "validations" do
+    should validate_presence_of(:name)
+    should validate_presence_of(:description)
+    should validate_presence_of(:subdomain)
+    should validate_presence_of(:domain)
+  end
+
+  context "database_columns" do
+    should have_db_column(:name)
+    should have_db_column(:description)
+    should have_db_column(:subdomain)
+    should have_db_column(:domain)
+    should have_db_column(:created_at)
+    should have_db_column(:updated_at)
+  end
+
+  test "location has_location? method" do
+    assert(@company.methods.include?(:has_locations?))
+  end
+
+  test "company.has_location? returns boolean" do
+    assert_includes([true, false], @company.has_locations?)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,24 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+require "simplecov"
+
+SimpleCov.start do
+  add_group "Active Admin", ["/app/admin/"]
+  add_group "Model",        "app/model"
+  add_group "Controllers",  "app/controllers"
+  add_group "Helpers",      "app/helpers"
+  add_group "Jobs",         "app/jobs"
+  add_group "Mailers",      "app/mailers"
+  add_group "Serializers",  "app/serializers"
+  add_group "Policies",     "app/policies"
+  add_group "Views",        "app/views"
+  add_group "Libraries",    "lib"
+
+end
 
 class ActiveSupport::TestCase
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -10,4 +26,11 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
+  end
 end
