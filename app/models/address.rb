02ba3d722d7e765784d4fb982c330efe6ed2823e
Jason Rossi <jason.rossi@epi.io>
2022-03-30 16:42:10 -0600

    update phone

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
   validates :label, :address_line_1, :city, :state, :zip, presence: true
+
+  # def display
+  #   ([label, address_line_1, address_line_2, city, state, zip, country].compact.reject {|element| element == "" }).join(', ')
+  # end
 end
--- lib/utils/phone.rb
+++ lib/utils/phone.rb
@@ -1,33 +1,42 @@
 class Utils::Phone
 
-  def self.validate(phone_number, type="slim")
-    phone_object = TelephoneNumber.parse(sanitize(phone_number), :US)
+  def self.validate(phone_number, validate_type = "slim")
+    if TelephoneNumber.parse(sanitize(phone_number)).country.nil?
+      phone_object = TelephoneNumber.parse(sanitize(phone_number), :PR).valid? ? TelephoneNumber.parse(sanitize(phone_number), :PR) : TelephoneNumber.parse(sanitize(phone_number), :US)
+    else
+      phone_object = TelephoneNumber.parse(sanitize(phone_number))
+    end
     result = {
       "valid" => phone_object.valid?,
       "raw_input" => phone_object.original_number,
       "national_number" => phone_object.national_number,
+      "international_number" => phone_object.international_number,
       "e164_number" => phone_object.e164_number,
       "country_code" => phone_object.country.country_code,
       "country_id" => phone_object.country.country_id,
       "location" => phone_object.location,
       "timezone" => phone_object.timezone
     }
-    case type
+    case validate_type
     when "all"
       result
     when "slim"
-      result.reject { |k,v| %w[country_code country_id location timezone].include? k }
+      result.reject { |k,v| %w[country_code location timezone].include? k }
     when "api"
-      result.reject { |k,v| %w[national_number country_code country_id location timezone e164_number].include? k }
+      result.reject { |k,v| %w[national_number international_number country_id country_code location timezone e164_number].include? k }
     else
       result
     end
   end
 
-  def self.normalize(phone_number, type = "national_number")
+  def self.normalize(phone_number, normalize_type = "national_number")
     validated = validate(phone_number)
-    result = validated["valid"] == true ? validated["national_number"] : validated["raw_input"]
-    case type
+    if ["US","PR"].include? validated["country_id"]
+      result = validated["valid"] == true ? validated["national_number"] : validated["raw_input"]
+    else
+      result = validated["valid"] == true ? validated["international_number"] : validated["raw_input"]
+    end
+    case normalize_type
     when "national_number"
       result
     when "e164_number"
