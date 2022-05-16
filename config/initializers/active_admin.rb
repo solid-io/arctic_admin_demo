995e683f6518dd0385b60f735962e62027d71552
Jason Rossi <jason.rossi@epi.io>
2022-05-16 13:48:36 -0600

    custom menu

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -266,11 +266,13 @@ ActiveAdmin.setup do |config|
   #
   # If you wanted to add a static menu item to the default menu provided:
   #
-  #   config.namespace :admin do |admin|
-  #     admin.build_menu :default do |menu|
-  #       menu.add label: "My Great Website", url: "http://www.mygreatwebsite.com", html_options: { target: :blank }
-  #     end
-  #   end
+    # config.namespace :admin do |admin|
+    #   admin.build_menu :default do |menu|
+    #     menu.add :label => "Developer" do |dev|
+    #       dev.add :label => "SimpleCov Report", :url => "open coverage/index.html", :html_options => { :target => :blank }
+    #     end
+    #   end
+    # end
 
   # == Download Links
   #
